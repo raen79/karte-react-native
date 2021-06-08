9f53ad16820681aa40ada3eb42cd3c63153b35ad
wasnot <wasnot.apps@gmail.com>
2021-06-08 15:43:34 +0900

    feat: android vt support

--- packages/visual-tracking/android/src/main/java/io/karte/react/visual_tracking/KarteVisualTrackingModule.kt
+++ packages/visual-tracking/android/src/main/java/io/karte/react/visual_tracking/KarteVisualTrackingModule.kt
@@ -15,40 +15,76 @@
 //
 package io.karte.react.visual_tracking
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.*
+import com.facebook.react.uimanager.UIManagerModule
+import com.facebook.react.uimanager.events.TouchEventType
+import io.karte.android.core.logger.Logger
 import io.karte.android.visualtracking.BasicAction
+import io.karte.android.visualtracking.ImageProvider
 import io.karte.android.visualtracking.VisualTracking
 import io.karte.android.visualtracking.VisualTrackingDelegate
 
-class KarteVisualTrackingModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
-  private var isPaired : Boolean = VisualTracking.isPaired
+private const val LOG_TAG = "Karte.VT.RN"
+
+class KarteVisualTrackingModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext), LifecycleEventListener {
+  private var isPaired: Boolean = VisualTracking.isPaired
+  private var isRegistered = false
 
   init {
-     VisualTracking.delegate = object : VisualTrackingDelegate(){
-       override fun onDevicePairingStatusUpdated(isPaired: Boolean) {
-         super.onDevicePairingStatusUpdated(isPaired)
-       }
-     }
+    VisualTracking.delegate = object : VisualTrackingDelegate() {
+      override fun onDevicePairingStatusUpdated(isPaired: Boolean) {
+        super.onDevicePairingStatusUpdated(isPaired)
+        this@KarteVisualTrackingModule.isPaired = isPaired
+      }
+    }
+    reactContext.addLifecycleEventListener(this)
   }
 
-    override fun getName(): String {
-        return "RNKRTVisualTracking"
+  override fun getName(): String {
+    return "RNKRTVisualTracking"
+  }
+
+  override fun onHostResume() {
+    addListenerIfNeed()
+  }
+
+  override fun onHostPause() {
+  }
+
+  override fun onHostDestroy() {
+  }
+
+  private fun addListenerIfNeed() {
+    if (isRegistered) return
+    isRegistered = true
+    val uiManagerModule: UIManagerModule = reactApplicationContext.getNativeModule(UIManagerModule::class.java)
+    uiManagerModule.eventDispatcher.addListener { event ->
+//      Logger.d(LOG_TAG, "onEventDispatch, ${event.eventName}")
+      if (event.eventName == TouchEventType.getJSEventName(TouchEventType.END)) {
+        UiThreadUtil.runOnUiThread {
+          runCatching {
+            val v = uiManagerModule.resolveView(event.viewTag)
+            Logger.d(LOG_TAG, "onEventDispatch touchEnd, ${v.javaClass.simpleName}, ${event.viewTag} ${v.tag}")
+            val action = BasicAction("RN.touchEnd", v.actionId, v.targetText, ImageProvider { v.bitmap })
+            VisualTracking.handle(action)
+          }
+        }
+      }
     }
+  }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
 
-      promise.resolve(a * b)
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  fun multiply(a: Int, b: Int, promise: Promise) {
 
-    }
+    promise.resolve(a * b)
+
+  }
 
   @ReactMethod
-  fun handle(action:String, actionId: String, targetText: String) {
+  fun handle(action: String, actionId: String, targetText: String) {
     VisualTracking.handle(BasicAction(action, actionId, targetText))
   }
 
